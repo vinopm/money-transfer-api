552570c704fe62d31d876f4cb54e446a8438aef7
vinopm <vino.m@me.com>
2019-04-17 09:59:21 +0100

    cleanup

--- src/main/java/com/revolut/rest/Request.java
+++ src/main/java/com/revolut/rest/Request.java
@@ -9,11 +9,11 @@ import java.net.URI;
 import java.util.Map;
 
 public interface Request {
-    public Headers getRequestHeaders () ;
-    public URI getRequestURI () ;
-    public String getRequestMethod ();
-    public HttpContext getHttpContext ();
-    public InputStream getRequestBody () ;
-    public String getProtocol ();
-    public InetSocketAddress getRemoteAddress ();
+    Headers getRequestHeaders() ;
+    URI getRequestURI() ;
+    String getRequestMethod();
+    HttpContext getHttpContext();
+    InputStream getRequestBody() ;
+    String getProtocol();
+    InetSocketAddress getRemoteAddress();
 }
